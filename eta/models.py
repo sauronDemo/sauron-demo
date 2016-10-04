86cbaac9c3ac2fd0a4d6e22ff56d7d846c5c7f4b
Kefu Zhao <kzhao@zenefits.com>
2016-10-03 20:58:24 -0700

    remove eta

--- eta/models.py
+++
@@ -1,9 +0,0 @@
-from django.models import ExtendedModel
-
-class Eta(ExtendedModel):
-	foo = CompressedSignatureField()
-	bar = models.DateTimeField(auto_now_add=True)
-
-	
-	class Meta:
-		db_table = 'abc'
